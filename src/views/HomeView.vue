1765a09033f47b48bff42f0e52ea87dba875cacb
Jackson <ageofwushud@gmail.com>
2024-12-05 11:53:09 +0700

    transaction vue3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,27 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const show = ref(true)
+</script>
 
 <template>
   <main>
     <h1>Home</h1>
+    <button @click="show = !show">Toggle</button>
+    <Transition>
+      <p v-if="show">hello</p>
+    </Transition>
   </main>
 </template>
+
+<style>
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
